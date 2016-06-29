b3724805376d5f08d9cc1a2bd824c63396afb7bb
LepkoQQ <lepko.san@gmail.com>
2016-06-29 02:02:33 +0200

    rebundle

--- bundle.cmd
+++ bundle.cmd
@@ -1,6 +1,6 @@
 ::
-call uglifycss css\custom.css > dist\custom.min.css
+del dist\* /Q
 ::
-call uglifyjs js\jquery.min.js js\countdown.min.js js\main.js -o dist\bundle.min.js -c -m
-::
+call uglifycss css\custom.css > dist\custom2.min.css
+call uglifyjs js\jquery.min.js js\countdown.min.js js\main.js -o dist\bundle2.min.js -c -m
 xcopy css\bootstrap.min.css dist\bootstrap.min.css* /Y
